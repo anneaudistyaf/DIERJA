1982a633673a6179968f176df50ec61e423bec3e
dzikal45 <dzikal45@gmail.com>
2021-05-28 22:33:09 +0700

    asu asu

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -3,12 +3,13 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 class HttpsProtocol
 {
 
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if (!$request->secure() && App::environment() === 'production') {
             return redirect()->secure($request->getRequestUri());
