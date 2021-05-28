edcfd12076df4ac59dd13d375aaedbf7213ea1d3
anneaudistyaf <anneaudistyaf@gmail.com>
2021-05-28 19:47:10 +0700

    update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,8 +8,14 @@
     <title>@yield('profile')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" type="text/css" href="{{url('../css/profile.css')}}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -87,7 +93,7 @@
                             <th scope="col">No</th>
                             <th scope="col">Nama Perusahaan</th>
                             <th scope="col">Posisi</th>
-                            <th scope="col">Status Lebaran</th>
+                            <th scope="col">Status Lamaran</th>
                         </tr>
                     </thead>
                     <tbody>
